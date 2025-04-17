30cd23ceaf1d4552d7c4387cd7a8869a42d1d044
alfkristian02 <alfkristian02@gmail.com>
2025-04-17 12:31:13 +0200

    feat: [issue #6] added a collection page for url not in use

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import Header from './components/header.tsx'
 import { Route, Routes} from 'react-router-dom'
 import Home from './pages/home.tsx'
 import Random from './pages/random.tsx'
 import Projects from './pages/projects.tsx'
+import NotFound from './pages/notfound.tsx'
+import Header from "./components/header.tsx"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/random" element={<Random />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   )
---
+++ src/pages/notfound.tsx
@@ -0,0 +1,9 @@
+function notfound () {
+    return (
+        <>
+            <h1 className="text-xl text-center mt-16">Beklager, denne siden finnes ikke:/</h1>
+        </>
+    )
+}
+
+export default notfound
